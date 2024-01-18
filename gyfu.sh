ce53c98de37e1fa3a3efce5ae47ff24e8ea1d714
josiah <josiah.chapman@gmail.com>
2024-01-17 19:10:49 -0800

    (~)

--- gyfu.sh
+++ gyfu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG="0.20"
+TAG="0.21"
 
 # build image
 docker buildx build --platform linux/amd64 -t josiahdc/trail:${TAG} ./trail --push
--- trail/src/trail.py
+++ trail/src/trail.py
@@ -1,4 +1,3 @@
-import subprocess
 from time import sleep
 
 import requests as requests
@@ -7,52 +6,22 @@ from loguru import logger
 
 MAX_ATTEMPTS = 7
 
+app = Flask(__name__)
+
 
 class Trail:
     def __init__(self):
         logger.info("( ) initializing trail")
-        self.config_path = "/bough/openvpn/config"
-        self.auth_path = "/bough/openvpn/auth"
         self.reflector_url = "https://api.ipify.org?format=json"
         self.overhill_address = self.collect_overhill_address()
-        self.connect_openvpn()
-        self.force_cloudflare_dns()
-        self.start_proxy()
         self.confirm_connection()
-
-        # prep flask
-        self.flask_app = Flask("trail")
-        self.flask_app.add_url_rule("/condemn", view_func=self.condemn, methods=["POST"])
         logger.info("(*) initialized trail")
-
-    def condemn(self):
-        logger.info("( ) shutting down")
-        raise Exception("Shutdown!")
 
     def collect_overhill_address(self):
         logger.info(f"( ) collecting overhill address")
         overhill_address = requests.get(self.reflector_url).json()["ip"]
         logger.info(f"(*) collected overhill address: {overhill_address}")
         return overhill_address
-
-    def connect_openvpn(self):
-        logger.info("( ) starting openvpn")
-        command = f"nohup openvpn --config {self.config_path} --auth-user-pass {self.auth_path} &"
-        self.shell_exec(command)
-        logger.info("(*) started openvpn")
-
-    def force_cloudflare_dns(self):
-        logger.info("( ) forcing cloudflare dns resolution")
-        command = "echo 'nameserver 1.1.1.1\nnameserver 1.0.0.1\noptions edns0 trust-ad' > /etc/resolv.conf"
-        self.shell_exec(command)
-        logger.info("(*) forced cloudflare dns resolution")
-
-    def start_proxy(self):
-        logger.info("( ) starting proxy")
-        command = "tinyproxy"
-        # (~) if you want to debug, use `tinyproxy -d` instead
-        self.shell_exec(command)
-        logger.info("(*) started proxy")
 
     def confirm_connection(self):
         logger.info("( ) confirming disguise")
@@ -74,22 +43,3 @@ class Trail:
         if current_attempts >= MAX_ATTEMPTS:
             raise Exception("Could not verify ipv4 address had changed (~) check vpn")
         logger.info(f"(*) confirmed disguise: {self.overhill_address} -> {underhill_address}")
-
-    def shell_exec(self, command):
-        subprocess.run(
-            command,
-            shell=True,
-            encoding="UTF-8",
-            check=True,
-            capture_output=False
-        )
-
-
-def create_app():
-    trail = Trail()
-    return trail.flask_app
-
-
-if __name__ == '__main__':
-    trail = Trail()
-    trail.flask_app.run(host="0.0.0.0", port=33700, threaded=False, processes=1)
