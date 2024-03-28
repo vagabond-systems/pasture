d9fdedf3bf9d2f7d5ac4adf30c33fdb73b6fd42b
josiah <josiah.chapman@gmail.com>
2024-03-28 13:05:51 -0700

    (~)

--- zenith/scripts/bootstrapper.sh
+++ zenith/scripts/bootstrapper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cp /etc/openvpn/resolv.conf /etc/resolv.conf
 . /app/scripts/restart_openvpn.sh
 sleep infinity
--- zenith/scripts/restart_openvpn.sh
+++ zenith/scripts/restart_openvpn.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-if [ -f /tmp/openvpn_pid.txt ]; then
-    kill "$(cat /tmp/openvpn_pid.txt)"
-fi
 RANDOM_CONFIG_PATH=$(find /etc/openvpn/configs/ -type f -regex ".*\.ovpn$" | shuf -n 1)
 echo "$RANDOM_CONFIG_PATH"
-ORIGINAL_IP=$(curl icanhazip.com)
 nohup openvpn --config "$RANDOM_CONFIG_PATH" --auth-user-pass /etc/openvpn/login.conf &
 echo $! >/tmp/openvpn_pid.txt
-sleep 5
-NEW_IP=$(curl icanhazip.com)
-echo "(*) initial ip:"
-echo "$ORIGINAL_IP"
-echo "(*) turned into:"
-echo "$NEW_IP"
-if [ "$NEW_IP" == "$ORIGINAL_IP" ]; then
-    exit
-fi
