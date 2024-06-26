fca52aee5ffe3d141fa3d274332ee41df06aec76
josiah <josiah.chapman@gmail.com>
2024-06-25 17:56:10 -0700

    (~)

--- gyfu.sh
+++ gyfu.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-NO_CACHE=true
+NO_CACHE=false
 
 declare -a PLATFORMS=(
     "linux/amd64"
     #     "linux/arm64/v8"
 )
-
-if [ "$NO_CACHE" = true ]; then
-    NO_CACHE="--no-cache=true"
-    echo "building without cache"
-else
-    NO_CACHE=""
-    echo "building with cache"
-fi
-
-{
-    read -r TAG
-} <version.txt
-
 for PLATFORM in "${PLATFORMS[@]}"; do
-    docker buildx build "$NO_CACHE" --platform "$PLATFORM" -t josiahdc/trailhead:"${TAG}" --push ./trailhead
-    docker buildx build "$NO_CACHE" --platform "$PLATFORM" -t josiahdc/switchback:"${TAG}" --push ./switchback
-    docker buildx build "$NO_CACHE" --platform "$PLATFORM" -t josiahdc/zenith:"${TAG}" --push ./zenith
-    docker buildx build "$NO_CACHE" --platform "$PLATFORM" -t josiahdc/cartographer:"${TAG}" --push --build-arg UNDERHILL_TAG="${TAG}" ./cartographer
-    docker buildx build "$NO_CACHE" --platform "$PLATFORM" -t josiahdc/pathfinder:"${TAG}" --push ./pathfinder
+    BUILD_OPTIONS="--platform $PLATFORM --push"
+    if [ "$NO_CACHE" = true ]; then
+        BUILD_OPTIONS+=" --no-cache=true"
+        echo "building without cache"
+    else
+        echo "building with cache"
+    fi
+
+    {
+        read -r TAG
+    } <version.txt
+
+    docker buildx build $BUILD_OPTIONS -t josiahdc/trailhead:"${TAG}" ./trailhead
+    docker buildx build $BUILD_OPTIONS -t josiahdc/switchback:"${TAG}" ./switchback
+    docker buildx build $BUILD_OPTIONS -t josiahdc/zenith:"${TAG}" ./zenith
+    docker buildx build $BUILD_OPTIONS -t josiahdc/cartographer:"${TAG}" --build-arg UNDERHILL_TAG="${TAG}" ./cartographer
+    docker buildx build $BUILD_OPTIONS -t josiahdc/pathfinder:"${TAG}" ./pathfinder
 done
